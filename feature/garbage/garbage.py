56d92c3a016c385c8893bf19f50b216ac9b6a329
沈晓敏 <shenxm@sq108.com>
2020-05-30 11:27:37 +0800

    Add - garbage

---
+++ feature/garbage/garbage.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Blueprint, jsonify, request
+
+garbage_blue_print = Blueprint('garbage', __name__)
+
+
+@garbage_blue_print.route('/intelligentScale/yz', methods=['POST', 'GET'])
+def yz():
+    card_num = request.values.get('card_num')
+    imei = request.values.get('IMEI')
+    if None in {card_num, imei}:
+        return jsonify(
+            {'code': 0}
+        )
+
+    return jsonify({
+        'code': 1,
+        'access_token': 12345678,
+        'owner_name': '我是大帅哥',
+        'garbage_type': '厨余垃圾'
+    })
+
+
+@garbage_blue_print.route('/intelligentScale/xl', methods=['POST'])
+def xl():
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    access_token = request.values.get('access_token')
+    card_num = request.values.get('card_num')
+    imei = request.values.get('IMEI')
+    btn_result = request.values.get('btn_result')
+    photo = request.files.get('photo')
+    if None in {access_token, card_num, imei, btn_result, photo}:
+        return jsonify({
+            'success': 0
+        })
+
+    path = basedir + "/static/photo/"
+    file_path = path + photo.filename
+    photo.save(file_path)
+
+    return jsonify({
+        'success': 1
+    })
